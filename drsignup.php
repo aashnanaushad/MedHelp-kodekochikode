1b83d1064219e9fcf730904998807e758c3bd489
aashnanaushad <36255953+aashnanaushad@users.noreply.github.com>
2019-03-15 16:11:48 +0530

    Add files via upload

---
+++ drsignup.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "app";
+
+$conn = new \MySQLi($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("INSERT INTO doctor (Drname,Dept,Dremail,Drphno,Drpass) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssis",$_POST['Drname'],$_POST['Dept'],$_POST['Dremail'],$_POST['Drphno'],$_POST['Drpass']);
+$stmt->execute();
+
+echo "Signup Successful";
+$stmt->close();
+$conn->close();
+?>
